0b5be2c5a004cba3be0b47da7b5b0d81966d452d
Dieu Cao <dcao@pivotal.io>
2015-02-20 23:35:35 -0800

    Hardcoded the bosh-light ami and fixed up misc other stuff that wasn't working anymore

--- bosh-might.rb
+++ bosh-might.rb
@@ -3,14 +3,14 @@
 require 'rubygems'
 require 'aws-sdk'
 require 'colored'
-require 'open4'
 
 AWS.config(:access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
            :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'])
 
 ec2                 = AWS::EC2.new
 cf_release_version  = ARGV[0]
-ami_name            = `curl -s https://bosh-lite-build-artifacts.s3.amazonaws.com/ami/bosh-lite-ami.list |tail -1`.chop
+#ami_name            = `curl -s https://bosh-lite-build-artifacts.s3.amazonaws.com/ami/bosh-lite-ami.list |tail -1`.chop
+ami_name            = 'ami-80c8b0e8' #boshlite-2811 https://github.com/cloudfoundry/bosh-lite/blob/master/Vagrantfile#L20
 instance_type       = 'm3.xlarge'
 $ssh_username       = 'ubuntu'
 $prefix              = "bosh-might".blue_on_black + ": ".yellow
@@ -29,13 +29,8 @@ class String
     end
   end
 
-def term_out(arg,newline=true)
-  if newline
-    nl = "\n"
-  else
-    nl = ""
-  end
-  print $prefix + arg.white + nl
+def term_out(arg)
+  puts $prefix + arg.white
 end
 
 if cf_release_version == nil
@@ -74,10 +69,13 @@ instance = ec2.instances.create(:image_id        => ami_name,
                                   }
                                 ])
 term_out "Launching bosh-lite instance ..."
+
+# wait until battle station is fully operational
 sleep 1 until instance.status != :pending
 term_out "Launched instance #{instance.id}, status: #{instance.status}, public dns: #{instance.dns_name}, public ip: #{instance.ip_address}"
 exit 1 unless instance.status == :running
-sleep 5
+sleep 60 # yeah, running isn't really running
+#omg what a hack
 $ip_address = instance.ip_address
 
 def ssh_command(arg, output=false)
@@ -86,20 +84,14 @@ def ssh_command(arg, output=false)
   else
     suffix = "> /dev/null 2>&1"
   end
-  res = Open4::popen4("ssh -o \"StrictHostKeyChecking no\" #{$ssh_username}@#{$ip_address} 'export DEBIAN_FRONTEND=\"noninteractive\"; #{arg} #{suffix}'") do |pid, stdin, stdout, stderr|
-    if stdout.read.strip.include? 'Connection refused'
-      print "."
-      sleep 5
-      ssh_command arg
-    end
-  end
+  `ssh -o "StrictHostKeyChecking no" #{$ssh_username}@#{$ip_address} 'export DEBIAN_FRONTEND="noninteractive"; #{arg} #{suffix}'`
 end
 
-term_out "Attempting to SSH to instance",false
-ssh_command "sudo apt-get -y update"
-ssh_command "sudo apt-get -y update" #gotta fix this hack
-term_out "!"
+
+
+
 term_out "Install Git, libmysql, libpq"
+ssh_command "sudo apt-get -y update"
 ssh_command "sudo apt-get -q -y install git libmysqlclient-dev libpq-dev"
 term_out "Install Bundler"
 ssh_command "sudo gem install bundler --no-rdoc --no-ri"
@@ -110,13 +102,15 @@ ssh_command "mkdir workspace"
 term_out "Installing unzip"
 ssh_command "sudo apt-get -q -y install unzip"
 term_out "Download Spiff"
-ssh_command "wget https://github.com/cloudfoundry-incubator/spiff/releases/download/v1.0/spiff_linux_amd64.zip -P ~/workspace/"
+ssh_command "wget https://github.com/cloudfoundry-incubator/spiff/releases/download/v1.0.3/spiff_linux_amd64.zip -P ~/workspace/"
 term_out "Install Spiff"
 ssh_command "sudo unzip -oq ~/workspace/spiff_linux_amd64.zip -d /usr/local/bin/"
 term_out "Cloning into CF-Release"
 ssh_command "git clone https://www.github.com/cloudfoundry/cf-release ./workspace/cf-release"
 term_out "Target local bosh-lite"
 ssh_command "bosh -n target 127.0.0.1"
+ssh_command "sudo chown ubuntu:ubuntu /home/ubuntu/.bosh_config"
+ssh_command "sudo chown ubuntu:ubuntu /home/ubuntu/tmp"
 ssh_command "bosh -n login admin admin"
 term_out "Download bosh-lite stemcell"
 #TODO: should have a way to get the latest bosh-lite stemcell and download it instead of hard coding it
@@ -139,6 +133,7 @@ ssh_command "cd ~/workspace/cf-release; ./bosh-lite/make_manifest"
 term_out "Bosh Deploy"
 ssh_command "bosh -n deploy", true
 
+
 term_out "Launched: You can SSH to it with;"
 term_out "ssh #{$ssh_username}@#{instance.ip_address}"
-term_out "Remember to terminate after your'e done!"
+term_out "Remember to terminate after you're done!"
