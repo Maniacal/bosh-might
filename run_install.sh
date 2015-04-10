acbbd0c02e2886d4afd69430ab17c30e1a9f53ea
Maniacal <mike.glenney@gmail.com>
2015-04-10 08:42:40 -0700

    Added usage info and call out to bosh-might.rb

--- run_install.sh
+++ run_install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+#
+# Usage:   ./run_installer.sh <cf_release_version> <ip address>
 
 cf_release_version="$1"
 ip_address="$2"
@@ -10,4 +12,5 @@ gem install colored --no-ri --no-rdoc
 sed -i 's/5000m/10000m/g' /var/vcap/data/jobs/director/fake-job-template-version-*/config/nginx.conf
 monit restart director_nginx
 
-
+# Run bosh might
+ruby ./bosh-might.rb "$cf_release_version" "$ip_address"
